59766ca8602783a5cf7645ba14d168e02b65b35c
Michael Bach <migobach@gmail.com>
2018-05-28 16:57:33 -0600

    add crud action for paints

--- app/controllers/paints_controller.rb
+++ app/controllers/paints_controller.rb
@@ -10,4 +10,37 @@ class PaintsController < ApplicationController
   def new
     @paint = Paint.new
   end
+
+  def create
+    @paint = Paint.new(paints_params)
+    if @paint.save 
+      redirect_to paints_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @paint = Paint.find(params[:id])
+  end
+
+  def update
+    @paint = Paint.find(params[:id])
+    if @paint.update(paints_params)
+      redirect_to paint_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Paint.find(params[:id]).destroy
+    redirect_to paints_path
+  end
+
+  private
+
+  def paints_params
+    params.require(:paint).permit(:name, :finish, :price, :available)
+  end
 end
--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -10,4 +10,37 @@ class PartsController < ApplicationController
   def new
     @part = Part.new
   end
+
+  def create
+    @part = Part.new(parts_params)
+    if @part.save
+      redirect_to parts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @part = Part.find(params[:id])
+  end
+
+  def update
+    @part = Part.find(params[:id])
+    if @part.update(parts_params)
+      redirect_to parts_path
+    else
+      render :edit
+    end 
+  end
+
+  def destroy
+    Part.find(params[:id]).destroy
+    redirect_to parts_path
+  end
+
+private 
+
+  def parts_params
+    params.require(:part).permit(:name, :description, :price, :available)
+  end
 end
